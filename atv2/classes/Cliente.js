8109ffc0a83aee1205f07bfdd79b9f9548166649
Suedz01 <suedalex2@hotmail.com>
2022-12-22 17:37:49 -0300

    Descrição

---
+++ atv2/classes/Cliente.js
@@ -0,0 +1,21 @@
+class Cliente{
+    constructor(Nome,CPF,Saldo,Senha){ //sobrecarga
+        this.Nome = Nome;
+        this.CPF = CPF;
+        this.Saldo = Saldo;
+        this.Senha = Senha;
+    }
+
+    depositar(){
+        this.Saldo;
+    }
+
+    sacar(){
+
+    }
+
+    trocarSenha(){
+
+    }
+
+}export default Cliente;
